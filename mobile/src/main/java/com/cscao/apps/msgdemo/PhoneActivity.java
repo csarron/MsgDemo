e53d38bd028f09d36b0a48c38c66604145e87cb7
Arron Cao <qqcao.cs@gmail.com>
2016-01-18 00:14:29 -0500

    refactor: encapsulate GMS API to a module, significantly reducing code in both wear and mobile sides

--- mobile/src/main/java/com/cscao/apps/msgdemo/PhoneActivity.java
+++ mobile/src/main/java/com/cscao/apps/msgdemo/PhoneActivity.java
@@ -1,29 +1,17 @@
 package com.cscao.apps.msgdemo;
 
 import android.app.Activity;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.cscao.apps.gmswearapi.GmsApi;
 import com.cscao.apps.mlog.MLog;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.wearable.CapabilityApi;
-import com.google.android.gms.wearable.CapabilityInfo;
-import com.google.android.gms.wearable.MessageApi;
-import com.google.android.gms.wearable.MessageEvent;
-import com.google.android.gms.wearable.Node;
-import com.google.android.gms.wearable.Wearable;
 
-import java.util.Set;
-
-public class PhoneActivity extends Activity implements MessageApi.MessageListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class PhoneActivity extends Activity
+{
 
     private final String MESSAGE1_PATH = "/p11";
     private final String MESSAGE2_PATH = "/p22";
@@ -31,32 +19,47 @@ public class PhoneActivity extends Activity implements MessageApi.MessageListene
     private TextView mTextView;
     private View message1Button;
     private View message2Button;
-    private GoogleApiClient mPhoneApiClient;
-
-    private String mPhoneRemoteNodeId;
 
     private static final String MSG_CAPABILITY_NAME = "msg_capability";
-    private CapabilityApi.CapabilityListener mPhoneCapabilityListener;
-
     private Handler handler;
+
+    private GmsApi gmsApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.cscao.apps.msgdemo.R.layout.phone_activity);
+        setContentView(R.layout.phone_activity);
         MLog.init(this);
         handler = new Handler();
 
+        gmsApi = new GmsApi(this, handler, MSG_CAPABILITY_NAME);
+
+        gmsApi.setOnMessageReceivedListener(new GmsApi.OnMessageReceivedListener() {
+            @Override
+            public void onMessageReceived(final GmsApi.MsgEnt messageEvent) {
+
+                if (messageEvent.getPath().equals(MESSAGE1_PATH)) {
+
+                    updateUI(messageEvent, getString(R.string.received_message1));
+
+                } else if (messageEvent.getPath().equals(MESSAGE2_PATH)) {
+
+                    updateUI(messageEvent, getString(R.string.received_message2));
+                }
+            }
+        });
+
         mTextView = (TextView) findViewById(com.cscao.apps.msgdemo.R.id.textView);
-        message1Button = findViewById(com.cscao.apps.msgdemo.R.id.message1Button);
-        message2Button = findViewById(com.cscao.apps.msgdemo.R.id.message2Button);
+        message1Button = findViewById(R.id.message1Button);
+        message2Button = findViewById(R.id.message2Button);
 
         // Set message1Button onClickListener to send message 1
         message1Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MLog.d("send m1", null);
-                sendMsg(MESSAGE1_PATH, getString(com.cscao.apps.msgdemo.R.string.msg_info1).getBytes());
+                gmsApi.sendMsg(MESSAGE1_PATH, getString(R.string.msg_info1).getBytes());
+                gmsApi.showToast(getString(R.string.message_sent) + MESSAGE1_PATH, Toast.LENGTH_SHORT);
             }
         });
 
@@ -64,169 +67,39 @@ public class PhoneActivity extends Activity implements MessageApi.MessageListene
         message2Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendMsg(MESSAGE2_PATH, getString(com.cscao.apps.msgdemo.R.string.msg_info2).getBytes());
+                gmsApi.sendMsg(MESSAGE2_PATH, getString(R.string.msg_info2).getBytes());
+                gmsApi.showToast(getString(R.string.message_sent) + MESSAGE2_PATH, Toast.LENGTH_SHORT);
             }
         });
 
-        setupApiConnection();
     }
 
-    private void setupApiConnection() {
-        mPhoneCapabilityListener =
-                new CapabilityApi.CapabilityListener() {
-                    @Override
-                    public void onCapabilityChanged(CapabilityInfo capabilityInfo) {
-                        updateMsgCapability(capabilityInfo);
-                    }
-                };
-
-        // Create GoogleApiClient
-        mPhoneApiClient = new GoogleApiClient.Builder(getApplicationContext()).addConnectionCallbacks(this).addApi(Wearable.API).build();
-
-        Wearable.CapabilityApi.addCapabilityListener(mPhoneApiClient, mPhoneCapabilityListener, MSG_CAPABILITY_NAME);
-
-    }
-
-    private void updateMsgCapability(CapabilityInfo capabilityInfo) {
-        Set<Node> connectedNodes = capabilityInfo.getNodes();
-        mPhoneRemoteNodeId = pickBestNodeId(connectedNodes);
-    }
-
-    private String pickBestNodeId(Set<Node> nodes) {
-        String bestNodeId = null;
-        // Find a nearby node or pick one arbitrarily
-        for (Node node : nodes) {
-            if (node.isNearby()) {
-                return node.getId();
-            }
-            bestNodeId = node.getId();
-        }
-        return bestNodeId;
-    }
-
-    private void sendMsg(final String path, byte[] msg) {
-        Wearable.MessageApi.sendMessage(mPhoneApiClient, mPhoneRemoteNodeId, path, msg).setResultCallback(new ResultCallback<MessageApi.SendMessageResult>() {
+    private void updateUI(final GmsApi.MsgEnt messageEvent, final String uiText) {
+        handler.post(new Runnable() {
             @Override
-            public void onResult(@NonNull MessageApi.SendMessageResult sendMessageResult) {
-                if (sendMessageResult.getStatus().isSuccess())
-                    Toast.makeText(getApplication(), getString(com.cscao.apps.msgdemo.R.string.message_sent) + path, Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(getApplication(), getString(com.cscao.apps.msgdemo.R.string.error_message) + path, Toast.LENGTH_SHORT).show();
+            public void run() {
+                String display = uiText + "\n" + new String(messageEvent.getData());
+                MLog.d("display is:" + display);
+                mTextView.setText(display);
             }
         });
     }
-
-    @Override
-    public void onMessageReceived(final MessageEvent messageEvent) {
-        if (messageEvent.getPath().equals(MESSAGE1_PATH)) {
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    String display = getString(com.cscao.apps.msgdemo.R.string.received_message1) + "\n" + new String(messageEvent.getData());
-                    MLog.d("display is:" + display);
-
-                    mTextView.setText(display);
-                }
-            });
-        } else if (messageEvent.getPath().equals(MESSAGE2_PATH)) {
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    String display = getString(com.cscao.apps.msgdemo.R.string.received_message2) + "\n" + new String(messageEvent.getData());
-                    MLog.d("display is:" + display);
-
-                    mTextView.setText(display);
-                }
-            });
-        }
-    }
-
-    // from GoogleApiClient.ConnectionCallbacks
-    @Override
-    public void onConnected(Bundle bundle) {
-        MLog.d("onConnected");
-        // Register Message listeners
-        Wearable.MessageApi.addListener(mPhoneApiClient, this);
-
-        Wearable.CapabilityApi.getCapability(
-                mPhoneApiClient, MSG_CAPABILITY_NAME,
-                CapabilityApi.FILTER_REACHABLE).setResultCallback(
-                new ResultCallback<CapabilityApi.GetCapabilityResult>() {
-                    @Override
-                    public void onResult(@NonNull CapabilityApi.GetCapabilityResult result) {
-                        if (result.getStatus().isSuccess()) {
-                            updateMsgCapability(result.getCapability());
-                            if (result.getCapability().getNodes().size() > 0) {
-                                message1Button.setEnabled(true);
-                                message2Button.setEnabled(true);
-                            }
-                            MLog.d(result.getCapability().getNodes().toString());
-                        } else {
-                            MLog.d("Failed to get capabilities, "
-                                    + "status: "
-                                    + result.getStatus().getStatusMessage());
-                        }
-                    }
-                });
-
-//        Wearable.NodeApi.getConnectedNodes(mPhoneApiClient).setResultCallback(new ResultCallback<NodeApi.GetConnectedNodesResult>() {
-//            @Override
-//            public void onResult(@NonNull NodeApi.GetConnectedNodesResult getConnectedNodesResult) {
-//                if (getConnectedNodesResult.getStatus().isSuccess() && getConnectedNodesResult.getNodes().size() > 0) {
-//                    mPhoneRemoteNodeId = getConnectedNodesResult.getNodes().get(0).getId();
-//                    message1Button.setEnabled(true);
-//                    message2Button.setEnabled(true);
-//                }
-//            }
-//        });
-    }
-
-    //from GoogleApiClient.ConnectionCallbacks
-    @Override
-    public void onConnectionSuspended(int i) {
-        MLog.d("onConnectionSuspended");
-        message1Button.setEnabled(false);
-        message2Button.setEnabled(false);
-    }
-
-    // from GoogleApiClient.OnConnectionFailedListener
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        MLog.d("onConnectionFailed");
-        if (connectionResult.getErrorCode() == ConnectionResult.API_UNAVAILABLE)
-            Toast.makeText(getApplicationContext(), getString(com.cscao.apps.msgdemo.R.string.wearable_api_unavailable), Toast.LENGTH_LONG).show();
-    }
-
 
     @Override
     protected void onResume() {
         super.onResume();
         MLog.d("onResume");
-        int connectionResult = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(getApplicationContext());
 
-        if (connectionResult != ConnectionResult.SUCCESS) {
-            // Google Play Services is NOT available. Show appropriate error dialog
-            GoogleApiAvailability.getInstance().showErrorDialogFragment(getParent(), connectionResult, 0, new DialogInterface.OnCancelListener() {
-                @Override
-                public void onCancel(DialogInterface dialog) {
-                    finish();
-                }
-            });
-        } else {
-            MLog.d("connect mPhoneApiClient");
-            mPhoneApiClient.connect();
-        }
+        gmsApi.connect();
+        message1Button.setEnabled(true);
+        message2Button.setEnabled(true);
     }
 
     @Override
     protected void onPause() {
         MLog.d("onPause");
-        // Unregister Node and Message listeners, disconnect GoogleApiClient and disable buttons
-        Wearable.MessageApi.removeListener(mPhoneApiClient, this);
-        Wearable.CapabilityApi.removeCapabilityListener(mPhoneApiClient, mPhoneCapabilityListener, MSG_CAPABILITY_NAME);
 
-        mPhoneApiClient.disconnect();
-
+        gmsApi.disconnect();
         message1Button.setEnabled(false);
         message2Button.setEnabled(false);
 
--- wear/src/main/java/com/cscao/apps/msgdemo/WearActivity.java
+++ wear/src/main/java/com/cscao/apps/msgdemo/WearActivity.java
@@ -1,44 +1,28 @@
 package com.cscao.apps.msgdemo;
 
 import android.app.Activity;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.cscao.apps.gmswearapi.GmsApi;
 import com.cscao.apps.mlog.MLog;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.wearable.CapabilityApi;
-import com.google.android.gms.wearable.CapabilityInfo;
-import com.google.android.gms.wearable.MessageApi;
-import com.google.android.gms.wearable.MessageEvent;
-import com.google.android.gms.wearable.Node;
-import com.google.android.gms.wearable.Wearable;
 
-import java.util.Set;
-
-public class WearActivity extends Activity implements MessageApi.MessageListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class WearActivity extends Activity {
 
     private final String MESSAGE1_PATH = "/p11";
     private final String MESSAGE2_PATH = "/p22";
 
     private TextView mTextView;
-    private GoogleApiClient mWearApiClient;
     private View message1Button;
     private View message2Button;
 
-    private String mWearNodeId;
-
     private static final String MSG_CAPABILITY_NAME = "msg_capability";
-    private CapabilityApi.CapabilityListener mWearCapabilityListener;
-
     private Handler handler;
+
+    private GmsApi gmsApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +31,24 @@ public class WearActivity extends Activity implements MessageApi.MessageListener
         MLog.init(this);
         handler = new Handler();
 
-        mTextView = (TextView) findViewById(R.id.textView);
+        gmsApi = new GmsApi(this, handler, MSG_CAPABILITY_NAME);
+
+        gmsApi.setOnMessageReceivedListener(new GmsApi.OnMessageReceivedListener() {
+            @Override
+            public void onMessageReceived(final GmsApi.MsgEnt messageEvent) {
+
+                if (messageEvent.getPath().equals(MESSAGE1_PATH)) {
+
+                    updateUI(messageEvent, getString(R.string.received_message1));
+
+                } else if (messageEvent.getPath().equals(MESSAGE2_PATH)) {
+
+                    updateUI(messageEvent, getString(R.string.received_message2));
+                }
+            }
+        });
+
+        mTextView = (TextView) findViewById(com.cscao.apps.msgdemo.R.id.textView);
         message1Button = findViewById(R.id.message1Button);
         message2Button = findViewById(R.id.message2Button);
 
@@ -55,7 +56,9 @@ public class WearActivity extends Activity implements MessageApi.MessageListener
         message1Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendMsg(MESSAGE1_PATH, getString(R.string.msg_info1).getBytes());
+                MLog.d("send m1", null);
+                gmsApi.sendMsg(MESSAGE1_PATH, getString(R.string.msg_info1).getBytes());
+                gmsApi.showToast(getString(R.string.message_sent) + MESSAGE1_PATH, Toast.LENGTH_SHORT);
             }
         });
 
@@ -63,177 +66,42 @@ public class WearActivity extends Activity implements MessageApi.MessageListener
         message2Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendMsg(MESSAGE2_PATH, getString(R.string.msg_info2).getBytes());
+                gmsApi.sendMsg(MESSAGE2_PATH, getString(R.string.msg_info2).getBytes());
+                gmsApi.showToast(getString(R.string.message_sent) + MESSAGE2_PATH, Toast.LENGTH_SHORT);
             }
         });
 
-        setupApiConnection();
     }
 
-    private void setupApiConnection() {
-        mWearCapabilityListener =
-                new CapabilityApi.CapabilityListener() {
-                    @Override
-                    public void onCapabilityChanged(CapabilityInfo capabilityInfo) {
-                        updateMsgCapability(capabilityInfo);
-                    }
-                };
-
-        // Create GoogleApiClient
-        mWearApiClient = new GoogleApiClient.Builder(getApplicationContext()).addConnectionCallbacks(this).addApi(Wearable.API).build();
-
-        Wearable.CapabilityApi.addCapabilityListener(mWearApiClient, mWearCapabilityListener, MSG_CAPABILITY_NAME);
-
-    }
-
-    private void updateMsgCapability(CapabilityInfo capabilityInfo) {
-        Set<Node> connectedNodes = capabilityInfo.getNodes();
-        mWearNodeId = pickBestNodeId(connectedNodes);
-    }
-
-    private String pickBestNodeId(Set<Node> nodes) {
-        String bestNodeId = null;
-        // Find a nearby node or pick one arbitrarily
-        for (Node node : nodes) {
-            if (node.isNearby()) {
-                return node.getId();
-            }
-            bestNodeId = node.getId();
-        }
-        return bestNodeId;
-    }
-
-    private void sendMsg(final String path, byte[] msg) {
-        Wearable.MessageApi.sendMessage(mWearApiClient, mWearNodeId, path, msg).setResultCallback(new ResultCallback<MessageApi.SendMessageResult>() {
+    private void updateUI(final GmsApi.MsgEnt messageEvent, final String uiText) {
+        handler.post(new Runnable() {
             @Override
-            public void onResult(@NonNull MessageApi.SendMessageResult sendMessageResult) {
-                if (sendMessageResult.getStatus().isSuccess())
-                    Toast.makeText(getApplication(), getString(R.string.message_sent) + path, Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(getApplication(), getString(R.string.error_message) + path, Toast.LENGTH_SHORT).show();
+            public void run() {
+                String display = uiText + "\n" + new String(messageEvent.getData());
+                MLog.d("display is:" + display);
+                mTextView.setText(display);
             }
         });
     }
-
-    @Override
-    public void onMessageReceived(final MessageEvent messageEvent) {
-        if (messageEvent.getPath().equals(MESSAGE1_PATH)) {
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    String display = getString(R.string.received_message1) + "\n" + new String(messageEvent.getData());
-                    MLog.d("display is:" + display);
-
-                    mTextView.setText(display);
-                }
-            });
-        } else if (messageEvent.getPath().equals(MESSAGE2_PATH)) {
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    String display = getString(R.string.received_message2) + "\n" + new String(messageEvent.getData());
-                    MLog.d("display is:" + display);
-
-                    mTextView.setText(display);
-                }
-            });
-        }
-    }
-
-    // from GoogleApiClient.ConnectionCallbacks
-    @Override
-    public void onConnected(Bundle bundle) {
-        MLog.d("onConnected");
-
-        // Register Message listeners
-        Wearable.MessageApi.addListener(mWearApiClient, this);
-
-        Wearable.CapabilityApi.getCapability(
-                mWearApiClient, MSG_CAPABILITY_NAME,
-                CapabilityApi.FILTER_REACHABLE).setResultCallback(
-                new ResultCallback<CapabilityApi.GetCapabilityResult>() {
-                    @Override
-                    public void onResult(@NonNull CapabilityApi.GetCapabilityResult result) {
-                        if (result.getStatus().isSuccess()) {
-                            updateMsgCapability(result.getCapability());
-                            if (result.getCapability().getNodes().size() > 0) {
-                                message1Button.setEnabled(true);
-                                message2Button.setEnabled(true);
-                            }
-                            MLog.d(result.getCapability().getNodes().toString());
-                        } else {
-                            MLog.d("Failed to get capabilities, "
-                                    + "status: "
-                                    + result.getStatus().getStatusMessage());
-                        }
-                    }
-                });
-
-        // If there is a connected node, get it's id that is used when sending messages
-//        Wearable.NodeApi.getConnectedNodes(mWearApiClient).setResultCallback(new ResultCallback<NodeApi.GetConnectedNodesResult>() {
-//            @Override
-//            public void onResult(@NonNull NodeApi.GetConnectedNodesResult getConnectedNodesResult) {
-//                if (getConnectedNodesResult.getStatus().isSuccess() && getConnectedNodesResult.getNodes().size() > 0) {
-//                    mWearNodeId = getConnectedNodesResult.getNodes().get(0).getId();
-//                    message1Button.setEnabled(true);
-//                    message2Button.setEnabled(true);
-//                }
-//            }
-//        });
-
-    }
-
-    //from GoogleApiClient.ConnectionCallbacks
-    @Override
-    public void onConnectionSuspended(int i) {
-        MLog.d("onConnectionSuspended");
-
-        message1Button.setEnabled(false);
-        message2Button.setEnabled(false);
-    }
-
-    // from GoogleApiClient.OnConnectionFailedListener
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        MLog.d("onConnectionFailed");
-        if (connectionResult.getErrorCode() == ConnectionResult.API_UNAVAILABLE)
-            Toast.makeText(getApplicationContext(), getString(R.string.wearable_api_unavailable), Toast.LENGTH_LONG).show();
-    }
-
 
     @Override
     protected void onResume() {
         super.onResume();
         MLog.d("onResume");
-        int connectionResult = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(getApplicationContext());
 
-        if (connectionResult != ConnectionResult.SUCCESS) {
-            // Google Play Services is NOT available. Show appropriate error dialog
-            GoogleApiAvailability.getInstance().showErrorDialogFragment(getParent(), connectionResult, 0, new DialogInterface.OnCancelListener() {
-                @Override
-                public void onCancel(DialogInterface dialog) {
-                    finish();
-                }
-            });
-        } else {
-            mWearApiClient.connect();
-        }
+        gmsApi.connect();
+        message1Button.setEnabled(true);
+        message2Button.setEnabled(true);
     }
 
     @Override
     protected void onPause() {
         MLog.d("onPause");
 
-        // Unregister Node and Message listeners, disconnect GoogleApiClient and disable buttons
-        Wearable.MessageApi.removeListener(mWearApiClient, this);
-        Wearable.CapabilityApi.removeCapabilityListener(mWearApiClient, mWearCapabilityListener, MSG_CAPABILITY_NAME);
-
-        mWearApiClient.disconnect();
-
+        gmsApi.disconnect();
         message1Button.setEnabled(false);
         message2Button.setEnabled(false);
 
         super.onPause();
     }
-
 }
